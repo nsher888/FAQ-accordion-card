ece136d3f485db1de6c1dd007e0b7bb975121c8d
Nikoloz Shervashidze <nikoshervashidze@gmail.com>
2022-10-08 18:49:47 +0400

    edit

--- index.js
+++ index.js
@@ -1,7 +1,13 @@
 const items = document.querySelectorAll(".item");
 
 items.forEach((faq) => {
-	faq.addEventListener("click", () => {
+	faq.addEventListener("click", (e) => {
+		let active = document.querySelectorAll(".item.active");
 		faq.classList.toggle("active");
+		active.forEach((e) => {
+			if (e.classList.contains("active")) {
+				e.classList.remove("active");
+			}
+		});
 	});
 });
